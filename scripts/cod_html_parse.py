71ef0786a50034cb39b7b4b94dae5a2ae35c201d
Alphonsce <varlamov.al@phystech.edu>
2025-04-15 21:08:41 +0300

    updated COD parser

--- scripts/cod_html_parse.py
+++ scripts/cod_html_parse.py
@@ -10,7 +10,7 @@ import rootutils
 
 rootutils.setup_root(os.path.abspath('./'), indicator=".project-root", pythonpath=True, dotenv=True, cwd=True)
 
-from src.cif_utils import get_smiles_from_id_on_web
+from src.cif_utils import get_smiles_from_id_on_web, get_smiles_from_chemical_name
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
@@ -30,20 +30,28 @@ if __name__ == "__main__":
     else:
         results_df = pd.DataFrame(columns=["id", "smiles", "common_name"])
 
-    for i, id in tqdm(enumerate(cifs_id), total=len(cifs_id), desc="Processing COD IDs"):
+    for i, id in tqdm(enumerate(cifs_id), total=len(cifs_id)):
         if id in results_df['id'].values:
             continue
 
         try:
             smiles, common_name = get_smiles_from_id_on_web(id)
+            if smiles is None and common_name is not None:
+                try:
+                    smiles = get_smiles_from_chemical_name(common_name)
+                    print(f"Successfully got smiles from name for {id}")
+                except Exception as e:
+                    print(f"Failed to get smiles from name for {id}: {e}")
+                    continue
+                
             new_row = pd.DataFrame({"id": [id], "smiles": [smiles], "common_name": [common_name]})
             results_df = pd.concat([results_df, new_row], ignore_index=True)
             results_df.to_csv(csv_path, index=False)
-            
-            time.sleep(random.uniform(0.2, 0.5))  # Random sleep
+            time.sleep(random.uniform(0.2, 0.5))  # Random sleep between 0.5 and 2.5 seconds
 
         except Exception as e:
             print(f"Failed for {id}: {e}")
             time.sleep(random.uniform(2, 5))  # Sleep longer after a failure
             continue
+
 
